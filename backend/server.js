f100f63a0c5e1285d205a12e71fb9ffc2f16b7bd
VenexCon <venexconsultant@outlook.com>
2023-01-29 22:31:18 +0000

    <deploy> server.js updated with static folder script

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 //imports
+const path = require("path");
 const express = require("express"); //commonjs module JS syntax
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -15,11 +16,25 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 //Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
 app.use(errorHandler);
+
+// Serve Frontend
+
+if (process.env.NODE_ENV === "production") {
+  //set build folder static
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html");
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json({ message: "Welcome to the support desk API" });
+  });
+}
 
 app.listen(port, () => {
   console.log(`Port ${port}`);
