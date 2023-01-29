73f54b877061da03945eb911093113c199889488
Connor <96823004+VenexCon@users.noreply.github.com>
2023-01-29 22:13:34 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to the support desk API" });
-});
 
 //Routes
 app.use("/api/users", require("./routes/userRoutes"));
